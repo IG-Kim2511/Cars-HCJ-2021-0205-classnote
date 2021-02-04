da2d2f1ba8f8cb034bcb5ba2582a0da30f7a4984
iikim <iikim2511@gmail.com>
2021-02-04 15:26:21 -0500

    // js 024 // back to nomal from menu mode, when I click any point on browser // remove '.change' , when I click any point on browser

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // js 019 
 // click menu btn and show menu
-// html with '.target', put '.change' class in there
+// html with '.target' in [menu, navbar, section] ....put '.change' class in there
 
 document.querySelector('.menu').addEventListener('click',()=>{
 
@@ -9,4 +9,21 @@ document.querySelector('.menu').addEventListener('click',()=>{
         item.classList.toggle('change')
 
     });
+});
+
+// js 024 
+// back to nomal from menu mode, when I click any point on browser
+// remove '.change' , when I click any point on browser
+
+document.querySelectorAll('.wrapper').forEach((item)=>{
+    item.addEventListener('click',()=>{
+
+        document.querySelectorAll('.target').forEach((item)=>{
+            item.classList.remove('change')
+            
+    });
+    
+
+    })
+
 });
